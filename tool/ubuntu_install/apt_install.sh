ed9f249a3c88f3da3b1f83a2540c238229a78d3d
qiaolei-qql <qiaoleide2008@126.com>
2014-06-18 11:49:24 +0800

    ubuntu 常用包安装

---
+++ tool/ubuntu_install/apt_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt-get install vim
+sudo apt-get install ctags
+sudo apt-get install cscope
+sudo apt-get install subversion
+sudo apt-get install git
+sudo apt-get install dos2unix
+sudo apt-get install build-essential
+sudo apt-get install gcc-4.4
+sudo apt-get install g++-4.4-multilib
+sudo  apt-get install lrzsz 
+sudo apt-get install expect
+sudo apt-get install corkscrew
+
